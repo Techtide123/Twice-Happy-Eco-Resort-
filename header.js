82d2977f358f5dc3e79464836d9775ddc10b4206
Your Name <subhankarrout251@gmail.com>
2025-10-19 20:25:49 +0530

    Refactor code structure for improved readability and maintainability

--- header.js
+++ header.js
@@ -67,7 +67,7 @@ document.write(`
                                 alt="Twice Happy Eco Resorts"></a>
                     </div>
                     <div class="main__right">
-                        <a href="room-details-1.html" class="theme-btn btn-style sm-btn fill"><span>Book Now</span></a>
+                        <a href="booking-form.html" class="theme-btn btn-style sm-btn fill"><span>Book Now</span></a>
                         <button class="theme-btn btn-style sm-btn fill menu__btn d-lg-none" data-bs-toggle="offcanvas"
                             data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                             <span><img src="assets/images/icon/menu-icon.svg" alt=""></span>
